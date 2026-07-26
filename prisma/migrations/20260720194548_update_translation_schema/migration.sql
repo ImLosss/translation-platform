9a785fc4636b5a6a5042f3fe38bc5146f9b71857
ImLosss <pahriansyahryan@gmail.com>
2026-07-27 01:26:17 +0800

    first commit

--- prisma/migrations/20260720194548_update_translation_schema/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "TranslationStatus" AS ENUM ('RUNNING', 'COMPLETED', 'ERROR');
-
--- AlterTable
-ALTER TABLE "Translation" ADD COLUMN     "status" "TranslationStatus" NOT NULL DEFAULT 'RUNNING';
--- src/glosary/dto/update-glosary.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateGlosaryDto } from './create-glosary.dto';
-
-export class UpdateGlosaryDto extends PartialType(CreateGlosaryDto) {}
