c6b286fd792948caa8f8c680783956a11ad8829e
ImLosss <pahriansyahryan@gmail.com>
2026-08-05 22:15:41 +0800

    update user

--- app/(panel)/translate/create/page.tsx
+++ app/(panel)/translate/create/page.tsx
@@ -1,9 +1,8 @@
 import FormClient from "@/app/components/translate/create/FormClient";
 import { api } from "@/app/lib/api";
-import FormUrlJob from "./FormUrlJob";
 
 export default async function AdminPage() {
-    const glosaries = await api("/glosary");
+    const glosaries = await api<any>("/glosary");
     return (
         <>
             <FormClient glosaries={glosaries} />
