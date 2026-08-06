e06cf77410276febb42fc60c07ba5d0ce9aa8906
ImLosss <pahriansyahryan@gmail.com>
2026-08-07 04:43:42 +0800

    update auth

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const API_URL = process.env.API_URL!; // TANPA NEXT_PUBLIC
 
@@ -29,6 +30,8 @@ export async function api<T>(
     },
     cache: "no-store",
   });
+
+  if (response.status === 401) redirect("/login");
 
   const data = await response.json();
 
