54b775babb4132638618d8ee17da06b0c5deb6c3
ImLosss <pahriansyahryan@gmail.com>
2026-09-07 21:03:32 +0800

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,10 +2,14 @@ import 'dotenv/config';
 import * as bcrypt from 'bcrypt';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
 const prisma = new PrismaClient({
   adapter,
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(
     private config: ConfigService,
   ) {
-    const adapter = new PrismaPg({
-      connectionString: config.getOrThrow<string>('DATABASE_URL'),
-    });
+    // const adapter = new PrismaPg({
+    //   connectionString: config.getOrThrow<string>('DATABASE_URL'),
+    // });
+    const adapter = new PrismaMariaDb(config.getOrThrow<string>('DATABASE_URL'));
 
     super({
       adapter,
