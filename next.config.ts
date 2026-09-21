534f10cc9f624d61102e4e933f7af2162c8d3d29
ImLosss <pahriansyahryan@gmail.com>
2026-09-21 20:59:29 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ export default withSentryConfig(nextConfig, {
   project: "subnova",
 
   // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+  silent: false,
 
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
